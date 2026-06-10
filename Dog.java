27ba9c67606e9d6cfe57b596c4377e4d49652508
Pranav-Senthil-Vadivel <pra.senthilvadivel@campus.mccd.edu>
2026-06-10 01:06:27 +0000

    Almost Done

--- Dog.java
+++ Dog.java
@@ -1,8 +1,8 @@
 public class Dog {
 
-    public String name;
-    public int age;
-    public String breed;
+    private String name;
+    private int age;
+    private String breed;
 
      public Dog(){ // default constructor
 
@@ -16,8 +16,45 @@ public class Dog {
             this.name = name;
             this.age = age;
             this.breed = breed;
+
+
+            // Experiment: 
+            //          The compile error was "The constructor Dog() is undefined" because after adding a paramaterized constructor, Java doesn't create the "Object.toString()"" method 
+            //          anymore
+
+            
         }
 
+        @Override
+            public String toString() {
+                return "Dog{name = '" + name + "', age = " + age + ", breed = '" + breed + "'}";
+                
+            }
+
+
+        public String getName() { 
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge(){
+            return age;
+        }
+
+        public void setAge(int age){
+            // simple gaurd (optional)
+            if (age >= 0) this.age = age;
+        }
+
+        public String getBreed(){
+            return breed;
+        }
+        public void setBreed(String breed){
+            this.breed = breed;
+        }
 }
 
 
